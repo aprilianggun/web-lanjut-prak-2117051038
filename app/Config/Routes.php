dfa988438cf2ea331019500bf5527605db2a6a33
aprilianggun <aprilia.anggun21@students.unila.ac.id>
2023-09-28 01:46:41 +0700

    susah

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
-# $routes->get('/profile/(:any)/(:any)/(:any)', 'UserController::profile/$1/$2/$3');
+$routes->get('/profile/(:any)/(:any)/(:any)', 'UserController::profile/$1/$2/$3');
+
+// Rute untuk menampilkan formulir pembuatan pengguna
+$routes->get('/user/create', 'UserController::create');
+// Rute untuk memproses formulir pembuatan pengguna
+$routes->post('/user/store', 'UserController::store');
